6ab96201a3e3d0fedb4bcce3be9c14ee4b95b496
Cristina González Marrero <43580219+cristicristi7@users.noreply.github.com>
2022-06-20 09:51:41 +0100

    change9

---
+++ src/components/PowerpuffGirl.js
@@ -0,0 +1,24 @@
+class PowerpuffGirl extends HTMLElement {
+    constructor() {
+        super();
+        this attachShadow({mode: "open"});
+    }
+    static get styles() {
+        /*return  
+        :host {
+
+        }
+        ;*/
+    }
+    connectedCallback() {
+        this render();
+    }
+
+    render() {
+        this shadowRoot this.innerHTML = ``
+        <style>{PowerpuffGirl styles}</style>
+        /*<div>
+
+        </div>`;*/
+    }
+}
